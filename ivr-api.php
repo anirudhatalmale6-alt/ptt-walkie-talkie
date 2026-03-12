b10b9068157e7536b12888947baaf313fe5a6f3a
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-03-12 11:55:02 +0000

    Add IVR API PHP file for Technoline PBX

---
+++ ivr-api.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * IVR API - Technoline PBX
+ *
+ * When a call reaches the API extension, the PBX sends a request to this URL.
+ * This script returns a JSON command to dial 0527172458.
+ *
+ * PBX Parameters (automatically appended to URL):
+ * - PBXphone: Caller's phone number
+ * - PBXnum: PBX number
+ * - PBXdid: DID number dialed
+ * - PBXcallId: Unique call ID
+ * - PBXcallType: in=incoming, out=outgoing campaign
+ * - PBXcallStatus: CALL=active, HANGUP=disconnected
+ * - PBXextensionId: Extension ID
+ * - PBXextensionPath: Extension path
+ */
+
+header('Content-Type: application/json; charset=utf-8');
+
+// Get PBX parameters
+$phone       = $_GET['PBXphone'] ?? '';
+$callId      = $_GET['PBXcallId'] ?? '';
+$callStatus  = $_GET['PBXcallStatus'] ?? '';
+$callType    = $_GET['PBXcallType'] ?? '';
+$extensionId = $_GET['PBXextensionId'] ?? '';
+
+// If call is disconnected - do nothing
+if ($callStatus === 'HANGUP') {
+    echo json_encode(["type" => "goTo", "goTo" => ""]);
+    exit;
+}
+
+// Dial 0527172458
+$response = [
+    "type"         => "simpleRouting",
+    "name"         => "dialResult",
+    "dialPhone"    => "0527172458",
+    "displayNumber"=> "",
+    "routingMusic" => "yes",
+    "ringSec"      => 30,
+    "limit"        => ""
+];
+
+echo json_encode($response, JSON_UNESCAPED_UNICODE);
